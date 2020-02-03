fbfb3b614bc5fd719be798df985e89173e3dc19c
poncho31 <gilles.pinchart@gmail.com>
2020-02-03 20:09:54 +0100

    route auth handling

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Auth::routes();
+Route::group(['middleware' => ['auth']], function () {
+
+        // Registration Routes...
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+
+        // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+});
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('language', 'HomeController@language')->name('language');
