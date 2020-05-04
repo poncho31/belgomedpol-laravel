021e60ea4625c5bb1786ee0dd9032b135f5a3a0c
Gilles Pinchart <gilles.pinchart@scabel.eu>
2020-05-04 20:40:21 +0200

    change db log message column type text

---
+++ database/migrations/2020_05_04_183528_update_message_logs_tabe.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateMessageLogsTabe extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            $table->text('message')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
