fc0f0859043db82c0b2e7b534931e413772bbaa9
Gilles Pinchart <gilles.pinchart@scabel.eu>
2020-10-30 14:19:43 +0100

    feature-2 change logs add aplication

--- app/Logs.php
+++ app/Logs.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Logs extends Model
 {
     public $timestamps = true;
-    protected $fillable = ['message','error', 'created_at', 'updated_at'];
+    protected $fillable = ['message','application','error', 'created_at', 'updated_at'];
 }
