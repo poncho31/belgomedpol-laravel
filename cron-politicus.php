835e5032147bc59ec8a4d2c7ebc0521b8ca912ae
Gilles Pinchart <gilles.pinchart@scabel.eu>
2020-12-15 12:29:45 +0100

    chdir rm

--- cron-politicus.php
+++ cron-politicus.php
@@ -1,6 +1,6 @@
 <?php
 echo "JOB BEGIN ". (date("Y-m-d H:i:s"))."\n";
 echo getcwd()."\n";
-chdir(getcwd());
+// chdir(getcwd());
 shell_exec("php artisan schedule:run");
 echo "JOB END". (date("Y-m-d H:i:s"))."\n";
